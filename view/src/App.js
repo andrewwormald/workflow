7885e4d7733936831c77a1e3f6f221c93d91941b
Andrew Wormald <andreww@luno.com>
2023-07-14 09:50:23 +0100

    add UI view boilerplate

---
+++ view/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BasicTable from "./Runs";
+
+function App() {
+    return (
+        <div>
+            <h1>Workflow</h1>
+            <div className="table-wrapper">
+                <BasicTable/>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default App;
