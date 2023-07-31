202a7c47cbbb9a7829ef5ae720f365367e8f18dc
Andrew Wormald <andreww@luno.com>
2023-07-31 16:03:03 +0100

    callback: Wrap error with foreign ID

--- callback.go
+++ callback.go
@@ -3,13 +3,17 @@ package workflow
 import (
 	"bytes"
 	"context"
+	"github.com/luno/jettison/errors"
+	"github.com/luno/jettison/j"
 	"io"
 )
 
 func processCallback[T any](ctx context.Context, w *Workflow[T], currentStatus, destinationStatus string, fn CallbackFunc[T], foreignID string, payload io.Reader) error {
 	runID, err := w.store.LastRunID(ctx, w.Name, foreignID)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to lookup last run id for callback", j.MKV{
+			"foreign_id": foreignID,
+		})
 	}
 
 	key := MakeKey(w.Name, foreignID, runID)
