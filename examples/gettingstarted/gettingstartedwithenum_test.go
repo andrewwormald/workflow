66bf89f0af1128fd746956caed4b0ee5bca71a2a
Andrew Wormald <andreww@luno.com>
2023-11-23 17:07:03 +0000

    update example structure

---
+++ examples/gettingstarted/gettingstartedwithenum_test.go
@@ -0,0 +1,47 @@
+package gettingstarted_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/luno/jettison/jtest"
+
+	"github.com/andrewwormald/workflow"
+	"github.com/andrewwormald/workflow/adapters/memrecordstore"
+	"github.com/andrewwormald/workflow/adapters/memrolescheduler"
+	"github.com/andrewwormald/workflow/adapters/memstreamer"
+	"github.com/andrewwormald/workflow/adapters/memtimeoutstore"
+	"github.com/andrewwormald/workflow/examples/gettingstarted"
+)
+
+func TestGettingStartedWithEnumWorkflow(t *testing.T) {
+	wf := gettingstarted.GettingStartedWithEnumWorkflow(gettingstarted.Deps{
+		EventStreamer: memstreamer.New(),
+		RecordStore:   memrecordstore.New(),
+		TimeoutStore:  memtimeoutstore.New(),
+		RoleScheduler: memrolescheduler.New(),
+	})
+	t.Cleanup(wf.Stop)
+
+	ctx := context.Background()
+	wf.Run(ctx)
+
+	foreignID := "82347982374982374"
+	runID, err := wf.Trigger(ctx, foreignID, gettingstarted.StatusStarted)
+	jtest.RequireNil(t, err)
+
+	workflow.Require(t, wf, gettingstarted.StatusReadTheDocs, foreignID, runID, gettingstarted.GettingStarted{
+		ReadTheDocs: "✅",
+	})
+
+	workflow.Require(t, wf, gettingstarted.StatusFollowedTheExample, foreignID, runID, gettingstarted.GettingStarted{
+		ReadTheDocs:     "✅",
+		FollowAnExample: "✅",
+	})
+
+	workflow.Require(t, wf, gettingstarted.StatusCreatedAFunExample, foreignID, runID, gettingstarted.GettingStarted{
+		ReadTheDocs:       "✅",
+		FollowAnExample:   "✅",
+		CreateAFunExample: "✅",
+	})
+}
